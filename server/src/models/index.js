6b81be55caa143961e4a510c7b98990e3f267ba3
cuchim <ngbao2485@gmail.com>
2025-12-07 21:14:26 +0700

    feat: add models index export

---
+++ server/src/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Room = require('./Room');
+const SketchHistory = require('./SketchHistory');
+const SessionParticipant = require('./SessionParticipant');
+
+module.exports = {
+  User,
+  Room,
+  SketchHistory,
+  SessionParticipant
+};
